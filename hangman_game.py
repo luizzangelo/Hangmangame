129c553b5a3f7dee31f1e89ae67cdce71ea70fb7
Luiz Angelo <luizangelo1919@gmail.com>
2022-09-14 20:45:19 -0300

    a partir daqui é sozinho

--- hangman_game.py
+++ hangman_game.py
@@ -85,4 +85,42 @@ class Hangman:
 #metodo para adivinhar a letra
     def guess(self, letter):
 
-    #
+#metodo para ver se acabou o jogo
+    def game_over(self):
+
+#metodo para ver se o jogador venceu
+    def game_win(self):
+
+#metodo para nao mostrar a letra
+    def hide_letter(self):
+
+#metodo para checar o status do game e imprimir o tabuleiro na tela
+    def print_game_status(self):
+
+#função para ler palavra de forma aleatoria
+def rand_word():
+    with open('palavras.txt', 'rt') as f:
+        banco = f.readlines()
+    return banco[random.randint(0, len(banco))].strip()
+
+#execução do programa
+
+def main():
+    game = Hangman(rand_word())
+
+    #enquanto o jogo nao tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+
+    #verifica o status do jogo
+    game.print_game_status()
+
+    #de acordo com o status, imprime a mensagem na tela para o usuario
+    if game.game_win():
+        print('\nParabéns! Você Venceu!!')
+    else:
+        print('\nGame over! Você perdeu.')
+        print('\nA palavra era' +game.word)
+    print('\nAgora vá estudar')
+
+#executa o programa
+if __name__ == '__main__':
+    main()
