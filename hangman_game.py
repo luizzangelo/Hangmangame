33d7bb9d4644a355ab13dee198a06fc1075e6b38
Luiz Angelo <luizangelo1919@gmail.com>
2022-09-14 22:16:51 -0300

    concluido

--- hangman_game.py
+++ hangman_game.py
@@ -69,39 +69,62 @@ O    |
 =========='''
 ]
 
-print('\nPalavra: ')
-
-print('\nLetras erradas: ')
-
-print('\nLetras corretas: ')
-
-print(input('\nDigite uma letra: '))
-
 class Hangman:
 
-#metodo construtor
+    #metodo construtor
     def __init__(self, word):
+        self.word = word
+        self.letra_errada = []
+        self.letra_certa = []
 
-#metodo para adivinhar a letra
+    #metodo para adivinhar a letra
     def guess(self, letter):
+        if letter in self.word and letter not in self.letra_certa:
+            self.letra_certa.append(letter)
+        elif letter not in self.word and letter not in self.letra_errada:
+            self.letra_errada.append(letter)
+        else:
+            return False
+        return True
 
-#metodo para ver se acabou o jogo
+    #metodo para ver se acabou o jogo
     def game_over(self):
+        return self.game_win() or (len(self.letra_errada)==6)
 
-#metodo para ver se o jogador venceu
+    #metodo para ver se o jogador venceu
     def game_win(self):
+        if '_' not in self.hide_letter():
+            return True
+        return False
 
-#metodo para nao mostrar a letra
+    #metodo para nao mostrar a letra no tabuleiro
     def hide_letter(self):
+        rtn =''
+        for letter in self.word:
+            if letter not in self.letra_certa:
+                rtn += '_'
+            else:
+                rtn += letter
+        return rtn
 
-#metodo para checar o status do game e imprimir o tabuleiro na tela
+    #metodo para checar o status do game e imprimir o tabuleiro na tela
     def print_game_status(self):
+        print(board[len(self.letra_errada)])
+        print('\nPalavra: ' + self.hide_letter())
+        print('\n Letras erradas: ',)
+        for letter in self.letra_errada:
+            print(letter,)
+        print ()
+        print('Letras corretas: ',)
+        for letter in self.letra_certa:
+            print(letter,)
+        print ()
 
 #função para ler palavra de forma aleatoria
 def rand_word():
     with open('palavras.txt', 'rt') as f:
-        banco = f.readlines()
-    return banco[random.randint(0, len(banco))].strip()
+        bank = f.readlines()
+    return bank[random.randint(0,len(bank))].strip()
 
 #execução do programa
 
@@ -109,6 +132,10 @@ def main():
     game = Hangman(rand_word())
 
     #enquanto o jogo nao tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+    while not game.game_over():
+        game.print_game_status()
+        entrada = input('\nDigite uma letra: ')
+        game.guess(entrada)
 
     #verifica o status do jogo
     game.print_game_status()
@@ -118,7 +145,7 @@ def main():
         print('\nParabéns! Você Venceu!!')
     else:
         print('\nGame over! Você perdeu.')
-        print('\nA palavra era' +game.word)
+        print('\nA palavra era' + game.word)
     print('\nAgora vá estudar')
 
 #executa o programa
