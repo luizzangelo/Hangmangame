e89c7230fbc25b708fca13bea495ad2fe445a63b
Luiz Angelo <luizangelo1919@gmail.com>
2022-09-14 23:18:55 -0300

    versão final com comentarios

--- hangman_game.py
+++ hangman_game.py
@@ -72,63 +72,65 @@ O    |
 class Hangman:
 
     #metodo construtor
-    def __init__(self, word):
-        self.word = word
+    def __init__(self, palavra): #inicia a classe
+        self.palavra = palavra
         self.letra_errada = []
         self.letra_certa = []
 
     #metodo para adivinhar a letra
-    def guess(self, letter):
-        if letter in self.word and letter not in self.letra_certa:
-            self.letra_certa.append(letter)
-        elif letter not in self.word and letter not in self.letra_errada:
-            self.letra_errada.append(letter)
+    def guess(self, letra):
+        if letra in self.palavra and letra not in self.letra_certa: #se a letra tiver na palavra e a letra nao estiver na lista de letra certa vai adicionar na lista de letra certa
+            self.letra_certa.append(letra)
+        elif letra not in self.palavra and letra not in self.letra_errada: #se a letra nao tiver na palavra e a letra nao estiver na lista de letra errada vai adicionar na lista de letra errada
+            self.letra_errada.append(letra)
         else:
             return False
         return True
 
     #metodo para ver se acabou o jogo
     def game_over(self):
-        return self.game_win() or (len(self.letra_errada)==6)
+        return self.game_win() or (len(self.letra_errada)==6) #quando a lista estiver com 6 itens na lista errada, perdemos
 
     #metodo para ver se o jogador venceu
     def game_win(self):
-        if '_' not in self.hide_letter():
+        if '_' not in self.hide_letter(): #quando nao tiver mais underline vencemos o jogo
             return True
         return False
 
     #metodo para nao mostrar a letra no tabuleiro
     def hide_letter(self):
         rtn =''
-        for letter in self.word:
-            if letter not in self.letra_certa:
+        for letra in self.palavra: #pra cada letra na sequencia de letras
+            if letra not in self.letra_certa: #se a letra nao estiver na palavra, preenche com underline
                 rtn += '_'
             else:
-                rtn += letter
+                rtn += letra #se estiver preenche com a letra
         return rtn
 
     #metodo para checar o status do game e imprimir o tabuleiro na tela
     def print_game_status(self):
-        print(board[len(self.letra_errada)])
-        print('\nPalavra: ' + self.hide_letter())
-        print('\n Letras erradas: ',)
-        for letter in self.letra_errada:
-            print(letter,)
-        print ()
-        print('Letras corretas: ',)
-        for letter in self.letra_certa:
-            print(letter,)
-        print ()
+        print(board[len(self.letra_errada)]) #vai trazer cada etapa da forca, quando estiver vazia, trás o primeiro elemento da lista board, e assim sucessivamente
+        print('\nPalavra: ' + self.hide_letter()) #pela primeira vez so chama underline
+        print('\n Letras erradas: ')
+        for letra in self.letra_errada: #para cada letra na lista de letras erradas, imprime a lista
+            print(letra)
+        print () #print linha vazia
+        print('Letras corretas: ')
+        for letra in self.letra_certa: #para cada letra na lista de letras certas, imprime a lista
+            print(letra)
+        print () #print linha vazia
 
 #função para ler palavra de forma aleatoria
 def rand_word():
     with open('palavras.txt', 'rt') as f:
-        bank = f.readlines()
-    return bank[random.randint(0,len(bank))].strip()
+        banco = f.readlines()
+    return banco[random.randint(0,len(banco))].strip()
 
 #execução do programa
 
 def main():
+
+    #objeto
     game = Hangman(rand_word())
 
     #enquanto o jogo nao tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
@@ -145,7 +147,7 @@ def main():
         print('\nParabéns! Você Venceu!!')
     else:
         print('\nGame over! Você perdeu.')
-        print('\nA palavra era' + game.word)
+        print('\nA palavra era ' + game.palavra)
     print('\nAgora vá estudar')
 
 #executa o programa
